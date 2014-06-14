b6c840442fae90b44c79d401da1bb516af49b550
whentp <tpsmsproject@gmail.com>
2014-06-15 01:29:31 +0800

    file extensions

--- src/common.h
+++ src/common.h
@@ -4,20 +4,20 @@
 #include "debug.h"
 
 namespace bible{
-
+	using namespace std;
 	class StopWatch {
-		private:
-			clock_t start;
-		public:
-			StopWatch();
-			double Stop();
+	private:
+		clock_t start;
+	public:
+		StopWatch();
+		double Stop();
 	};
 
 // file extensions.
-extern const char *file_ext_container_key;
-extern const char *file_ext_container_value;
-extern const char *file_ext_keyindex;
-extern const char *file_ext_compressedindex;
+	extern const char *file_ext_container_key;
+	extern const char *file_ext_container_value;
+	extern const char *file_ext_keyindex;
+	extern const char *file_ext_compressedindex;
 
 } // end namespace bible.
 
